2de7f6d3f166f414f72d815eb2bb42e935b567d4
vijayanuganti <vijayanuganti504@gmail.com>
2025-03-28 18:17:16 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Home from "./Components/Home"
+import Cabs from "./Components/Cabs"
+function App() {
+  
+
+  return (
+    <>
+    <Home/>
+    <Cabs/>
+
+    </>
+    
+  )
+}
+
+export default App
